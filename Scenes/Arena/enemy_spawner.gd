9f27edd4a9cea39a05e08abdca9c2a16e1962503
Vixen <Basov977@gmail.com>
2025-12-27 20:35:55 +0300

    enemy spawner updated

--- Scenes/Arena/enemy_spawner.gd
+++ Scenes/Arena/enemy_spawner.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@export var curve: Curve
+var number_of_enemy = 0
+
+@export var timer: Timer
+
 var distance_from_center = 10
 const ENEMIES = [
 	preload("uid://bigdgogdou3vp"), 
@@ -8,15 +13,19 @@ const ENEMIES = [
 ]
 
 func _ready() -> void:
-	_spawn_enemy(0)
-	_spawn_enemy(1)
-	_spawn_enemy(2)
+	timer.wait_time = curve.sample(0)
+	timer.start()
+	#_spawn_enemy(0)
+	#_spawn_enemy(1)
+	#_spawn_enemy(2)
 	#_spawn_enemy(2)
 
-func _spawn_enemy(index):
+func _spawn_enemy():
+	var index = randi_range(0, 2)
 	var pos = Vector3.RIGHT * distance_from_center
-	pos = pos.rotated(Vector3.UP, randf() * TAU)
+	pos = pos.rotated(Vector3.UP, randi_range(0, 3) * PI/2)
 	var temp = ENEMIES[index].instantiate()
 	temp.position = pos
 	add_child(temp)
-	
+	number_of_enemy += 1
+	timer.wait_time = curve.sample(number_of_enemy)
